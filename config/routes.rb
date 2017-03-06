5dd186a906354c51292d79a7fa6e9ea7a7b96690
Naiguel Santos <naiguel@criativitta.com.br>
2017-03-06 09:48:34 -0300

    form company and employees

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :employees
+  resources :companies do
+    resources :employees
+  end
+
+  root "companies#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
